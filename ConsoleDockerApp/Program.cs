e9110310f3c03e97ed9675d8c199f5a20e21d234
Serhat KARATAŞLI <serhatkaratasli790@gmail.com>
2023-06-12 12:52:05 +0300

    docker file docker image ve container oluşturuldu

---
+++ ConsoleDockerApp/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+int i = 0;
+while (i < 10000)
+{
+    Console.WriteLine("Hello, World! " + i.ToString());
+    i++;
+    System.Threading.Thread.Sleep(1000);
+
+
+}
